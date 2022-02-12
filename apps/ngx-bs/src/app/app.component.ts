d88f4fb444f540773966352103ef4f2fb3d183f1
matt <git@matttelliott.com>
2022-02-11 23:04:07 -0500

    initial commit

---
+++ apps/ngx-bs/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ngx-bs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ngx-bs';
+}
